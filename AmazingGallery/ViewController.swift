21556b3ae839c5dd0309acf38c17c9a09665f11f
nguyen thanh <thanh.nguyen2@sotatek.com>
2019-10-23 00:39:25 +0700

    Initial Commit

---
+++ AmazingGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AmazingGallery
+//
+//  Created by nguyenthanh on 10/23/19.
+//  Copyright © 2019 nguyenthanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
